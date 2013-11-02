1a5fa9f130c226eb80b93ce662f5aae7e0cc1131
Joe Kluchinski <joe.kluchinski@gmail.com>
2013-11-02 07:49:36 -0400

    debug

--- login/index.php
+++ login/index.php
@@ -28,18 +28,19 @@ else if ($action == 'check_login') {
     
     //Check for user
     $user = check_account($username, $password);
+    echo var_dump($user);
     //If user isn't found go back to login page and say user not found
    	
-    if($user == 'client'){
-        header("Location: /code-for-good-MHDC/client");
-    }
-    //If user is found and he is a customer
-    else if($user == 'admin'){
-        header("Location: /code-for-good-MHDC/employee");
-    }
-    else{
-    	header("Location: /code-for-good-MHDC/view/error.php");
-    }
+    // if($user == 'client'){
+    //     header("Location: /code-for-good-MHDC/client");
+    // }
+    // //If user is found and he is a customer
+    // else if($user == 'admin'){
+    //     header("Location: /code-for-good-MHDC/employee");
+    // }
+    // else{
+    // 	header("Location: /code-for-good-MHDC/view/error.php");
+    // }
 }
 //If the dropdown has been selected this runs
 //this will show the dropdown again as well as the tables
